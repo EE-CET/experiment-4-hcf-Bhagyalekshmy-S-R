075b0e1b1ac4ce63781f3d97ed39f8cea3670a48
Your Name <you@example.com>
2026-01-14 21:11:35 +0530

    first commit

--- src/HCF.java
+++ src/HCF.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class HCF {
     
-        // TODO: Read two integers and print their HCF
-    
+      public static  int hcf(int a,int b){
+               if(b==0){
+                    return a;
+                     }
+                else{
+                     return hcf(b,a%b);
+                  }
+    }
+    public static void main(String args[]){
+          Scanner sc=new Scanner(System.in);
+          int a =sc.nextInt();
+          int b= sc.nextInt();
+         
+          System.out.println(hcf(a,b));
+
+  }                 
 }
